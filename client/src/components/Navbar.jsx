87d6a02a3627424da4bd715ba1a37283d283d18b
Shubh Doshi <124681920+shubhdoshi21@users.noreply.github.com>
2024-09-02 17:04:06 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaSearch, FaBars } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
+import { BiLogIn } from "react-icons/bi";
 import ThemeConverter from "./ThemeConvertor";
+import Cookies from "js-cookie";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ onSidebarToggle }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Fetch the access token from cookies
+    const userToken = Cookies.get("accessToken");
+    console.log(userToken);
+
+    // Set login state based on the presence of the token
+    setIsLoggedIn(!!userToken);
+  }, []);
+
   return (
     <nav className="h-16 w-full flex items-center justify-between bg-white text-black p-4 shadow-lg bg-opacity-90 backdrop-blur-sm fixed z-50 top-0">
       <div className="text-xl font-bold">logo</div>
@@ -35,7 +49,7 @@ const Navbar = ({ onSidebarToggle }) => {
         </a>
       </div>
 
-      <div className="flex items-center ">
+      <div className="flex items-center">
         <div className="relative lg:block">
           <button className="absolute right-2 top-1/2 transform -translate-y-1/2">
             <FaSearch />
@@ -46,11 +60,21 @@ const Navbar = ({ onSidebarToggle }) => {
             className="pl-3 pr-10 py-1 rounded-full text-black bg-lightgrey"
           />
         </div>
+
         <div className="hidden sm:flex sm:items-center sm:justify-center sm:h-full sm:p-2">
-          <ThemeConverter/>
+          <ThemeConverter />
         </div>
+
         <div className="hidden sm:flex sm:items-center sm:justify-center sm:h-full sm:p-2">
-          <CgProfile size={24} />
+          {isLoggedIn ? (
+            <Link to="/profile">
+              <CgProfile size={24} />
+            </Link>
+          ) : (
+            <Link to="/auth/signin">
+              <BiLogIn size={24} />
+            </Link>
+          )}
         </div>
       </div>
 
