b26b074c3852ffd4f3219b5a00bfd24f558ce8c3
Shubh Doshi <124681920+shubhdoshi21@users.noreply.github.com>
2024-09-02 22:06:39 +0530

    Update Sidebar.jsx

--- client/src/components/Panel/Sidebar.jsx
+++ client/src/components/Panel/Sidebar.jsx
@@ -2,58 +2,51 @@ import React from "react";
 import SidebarLink from "./SidebarLink";
 import { sidebarlinks } from "../../data/SidebarLinks";
 import { useSelector } from "react-redux";
-import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { useDispatch } from "react-redux";
-import { setUserDetails, logoutUser } from "../../features/user/userSlice";
+import { logoutUser } from "../../features/user/userSlice";
 const Sidebar = () => {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user); 
-  
-    const [loading, setLoading] = useState(true);
-    const {userType} =useSelector((state)=>state.user)
-    const handleLogout = async () => {
-        try {
-          await axios.post(
-            "http://localhost:8080/api/v1/users/logout",
-            {},
-            { withCredentials: true }
-          );
-          dispatch(logoutUser());
-          toast.success("Logged out successfully!", {
-            autoClose: 1500,
-            closeButton: false,
-          });
-          setTimeout(() => {
-            window.location.href = "/auth/signin";
-          }, 1500);
-        } catch (err) {
-          toast.success(err.response?.data?.message || "Error logging out!.", {
-            autoClose: 1500,
-            closeButton: false,
-          });
-        }
-      };
-    console.log(userType)
+  const dispatch = useDispatch();
+  const { userType } = useSelector((state) => state.user);
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        "http://localhost:8080/api/v1/users/logout",
+        {},
+        { withCredentials: true }
+      );
+      dispatch(logoutUser());
+      toast.success("Logged out successfully!", {
+        autoClose: 1500,
+        closeButton: false,
+      });
+      setTimeout(() => {
+        window.location.href = "/auth/signin";
+      }, 1500);
+    } catch (err) {
+      toast.success(err.response?.data?.message || "Error logging out!.", {
+        autoClose: 1500,
+        closeButton: false,
+      });
+    }
+  };
+  console.log(userType);
   return (
-    <div>
-      <div className="flex w-[105%] text-white flex-col  border-r-richblack-700 h-[100%] bg-primaryPeach/80">
-        <div className="flex flex-col">
-          {sidebarlinks.map((link) => {
-            if (link.type && userType !== link.type) {
-              return null;
-            }
-            return (
-              <SidebarLink key={link.id} link={link} iconName={link.icon} />
-            );
-          })}
-        </div>
-        <div onClick={handleLogout} className="block p-2 cursor-pointer">
-              Logout
-            </div>
+    <div className="flex w-[15vw] text-white flex-col border-r-richblack-700 h-[100vh] fixed left-0 bg-primaryPeach/80">
+      <div className="flex flex-col">
+        {sidebarlinks.map((link) => {
+          if (link.type && userType !== link.type) {
+            return null;
+          }
+          return <SidebarLink key={link.id} link={link} iconName={link.icon} />;
+        })}
+      </div>
+      <div
+        onClick={handleLogout}
+        className="block p-4 cursor-pointer hover:bg-primaryPeach/80"
+      >
+        Logout
       </div>
     </div>
   );
