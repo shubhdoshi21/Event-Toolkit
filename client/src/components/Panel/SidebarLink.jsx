0b552e5946ed7900713a680634d31902a0b0dbdf
Shubh Doshi <124681920+shubhdoshi21@users.noreply.github.com>
2024-09-02 22:07:09 +0530

    Update SidebarLink.jsx

--- client/src/components/Panel/SidebarLink.jsx
+++ client/src/components/Panel/SidebarLink.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React from "react";
 import * as Icons from "react-icons/vsc";
-import { useDispatch } from 'react-redux';
-import { matchPath, NavLink, useLocation } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { matchPath, NavLink, useLocation } from "react-router-dom";
 
-const SidebarLink = ({link,iconName}) => {
-    const Icon = Icons[iconName] || Icons.VscQuestion;
-    const location = useLocation();
-    const matchRoute = (route)=>{
-
-        return matchPath({path:route},location.pathname);
-    }
+const SidebarLink = ({ link, iconName }) => {
+  const Icon = Icons[iconName] || Icons.VscQuestion;
+  const location = useLocation();
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname);
+  };
   return (
     <div>
-            <NavLink 
-                to={link.path} 
-                className={`relative flex items-center px-2 py-2 text-sm font-medium ${matchRoute(link.path) ? "bg-gray-400/30 w-full" : "bg-opacity-0"}`}
-            >
-                <span className={`absolute w-1 h-full left-0 ${matchRoute(link.path) ? "bg-lightGray" : "bg-opacity-0"}`} />
-                
-                <div className='flex items-center gap-x-2'>
-                    <Icon className="text-lg" />
-                    <span className='text-md'>
-                        {link.name}
-                    </span>
-                </div>
-            </NavLink>
-        </div>
-  )
-}
+      <NavLink
+        to={link.path}
+        className={`relative flex items-center p-4 text-sm font-medium ${
+          matchRoute(link.path) ? "bg-gray-400/30 w-full" : "bg-opacity-0"
+        }`}
+      >
+        <span
+          className={`absolute w-1 h-full left-0 ${
+            matchRoute(link.path) ? "bg-lightGray" : "bg-opacity-0"
+          }`}
+        />
 
-export default SidebarLink
+        <div className="flex items-center gap-x-2">
+          <Icon className="text-lg" />
+          <span className="text-md">{link.name}</span>
+        </div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default SidebarLink;
