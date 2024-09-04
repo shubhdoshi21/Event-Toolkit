3eee4be2a0c92ef6815d657d2099d78acf0abedb
Megh Prajapati <meghprajapati009@gmail.com>
2024-09-04 12:16:36 +0530

    made structure for venue and review store

---
+++ client/src/features/venue/venueSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+}
+
+const venueSlice = createSlice({
+    name: "venue",
+    initialState,
+    reducers: {
+
+    }
+})
+
+export const {} = venueSlice.actions;
+export default venueSlice.reducer;
