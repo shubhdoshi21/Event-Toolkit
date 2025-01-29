7c879700aa446bb015189f32de21e8f389ae48e1
Priyal208 <135015851+Priyal208@users.noreply.github.com>
2025-01-29 21:18:21 +0530

    Update cart.model.js

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -14,7 +14,10 @@ const cartSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
-        trim:true,
+        trim:true,  
+    },
+    image:{
+        type:String,
     },
     totalPrice:{
         type:Number,    
